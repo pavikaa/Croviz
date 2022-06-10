83cb3066409c16f94f940a0be2f39e71d5ca2be0
Marko Pavičić <mpavicic99@gmail.com>
2022-06-10 15:36:58 +0200

    First commit

---
+++ app/src/main/java/com/markopavicic/croviz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.markopavicic.croviz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
